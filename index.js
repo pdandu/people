941c5fac2294381618ea35f2dbb6deb6467e0bce
Pranathi <pdandu@indiana.edu>
2017-05-16 09:45:15 -0400

    Homework 1

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const personForm = document.querySelector('form')
 
-const renderColor = () => {
-const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = hairColor
-    colorDiv.style.height = '50px'
-    colorDiv.style.width = '100px'
-return colorDiv
+const renderColor = (hairColor) => {
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = hairColor
+  colorDiv.style.height = '50px'
+  colorDiv.style.width = '100px'
+  return colorDiv
 }
-const handleSubmit = (ev)
-personForm.onsubmit = (ev) =>
-{
-    ev.preventDefault()
-    const form = ev.target
-    const details = document.querySelector(".details")
 
-    const personName = form.personName.value
-    const hairColor = form.hairColor.value
-    const age = form.age.value
-    const birthplace = form.birthplace.value
+const handleSubmit = (ev) => {
+  ev.preventDefault()
+  const form = ev.target
+  const details = document.querySelector('.details')
 
-   const colorDiv = renderColor(hairColor)
+  const personName = document.createElement ('li')
+  personName.textContent = "Name: " + form.personName.value
+  
+  const hairColor = form.hairColor.value
+  const color = document.createElement ('li')
+  color.textContent = "Hair Color: "
 
-    const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = hairColor
-    colorDiv.style.height = '50px'
-    colorDiv.style.width = '100px'
+  const age = document.createElement ('li')
+  age.textContent = "Age: " + form.age.value 
 
-    details.innerHTML = `
-    <ul>
-        <li>Name: ${personName}</li>
-        <li>Hair Color: ${colorDiv.outerHTML}</li>
-        <li>Age: ${age}</li>
-        <li>Birthplace: ${birthplace}</li>
-    </ul>  
-  }  `
-   personForm.addEventListener<'submit', handleSubmit> 
+  const birthplace = document.createElement ('li')
+  birthplace.textContent = "Birthplace: " + form.birthplace.value 
+
+  const colorDiv = renderColor(hairColor)
+
+color.appendChild(colorDiv)
+details.appendChild(personName)
+details.appendChild(color)
+details.appendChild(age)
+details.appendChild(birthplace)
+
+ /* details.innerHTML = `
+    <ul>
+      <li>Name: ${personName}</li>
+      <li>Hair Color: ${colorDiv.outerHTML}</li>
+      <li>Age: ${age}</li>
+      <li>Birthplace: ${birthplace}</li>
+    </ul>
+  ` 
+*/
+}
+
+personForm.addEventListener('submit', handleSubmit)
+
